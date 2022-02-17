3e60d124ab38cecb9479ebdaa8655a9d9b862a19
Dimon1UA <92036555+Dimon1UA@users.noreply.github.com>
2022-02-17 17:35:48 +0200

    function

---
+++ main.js
@@ -0,0 +1,43 @@
+// function_1
+
+// function display_message() {
+//     alert('Мама, я програмист и изучаю LS!')
+// }
+// display_message()
+
+// function_2
+
+// function favorite_book(title) {
+//     let a = `One of my favorite book is ${title}`
+//     alert(a)
+// }
+// let b = prompt('Введите название своей любимой книги')
+// favorite_book(b)
+
+// function_3
+
+// function make_shirt(size, text) {
+//     let a = `Размер Вашей футболки ${size}, а текст на ней такой: ${text}`
+//     alert(a)
+// }
+// let b = +prompt('Введите размер Вашей футболки')
+// let c = prompt('Введите текст, который будет написан на Вашей футболке')
+// make_shirt(b,c)
+
+// function_4
+
+// function make_shirt(size, text) {
+//     let a = `Размер Вашей футболки ${size}, а текст на ней такой: ${text}`
+//     alert(a)
+// }
+// make_shirt('L','I love JS')
+
+// function_5
+
+// function describe_sity(sity, country) {
+//     let a = `${sity} is a ${country}`
+//     alert(a)
+// }
+// let b = +prompt('Введите название города, в котором вы живете')
+// let c = prompt('Введите страну, в которой вы живете')
+// describe_sity(b,c)
